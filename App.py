721ef2819161657f8c3459851f5164d3ece3bbf8
Tantt2026 <tantruong0305@gmail.com>
2026-09-23 13:46:16 +0700

    Update App.py

--- App.py
+++ App.py
@@ -1089,7 +1089,7 @@ st.markdown(f"""
 <div class="main-header">
     <div class="logo">{logo_svg}</div>
     <div class="title-block">
-        <h1>SƯ ĐOÀN HCM4 - TRUNG ĐOÀN 11 - TEST</h1>
+        <h1>SƯ ĐOÀN HCM4 - TRUNG ĐOÀN 11</h1>
         <h2>TRACKING KPI ĐDKD - TEAM SS NGUYỄN THỊ TƯỜNG VY </h2>
     </div>
 </div>
